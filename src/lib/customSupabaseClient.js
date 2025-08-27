3456851dfb5db9a3271b0d22a18c474d92ae22cf
unknown <GSTARTUP>
2025-08-27 17:52:23 +0100

    [dyad] Correction de la clé API Supabase - wrote 1 file(s)

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kdlxszgumbdbvybfgigy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI-rQA';
+const supabaseUrl = 'https://lueeptijffpwedouxylf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1ZWVwdGlqZmZwd2Vkb3V4eWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI3OTYsImV4cCI6MjA3MTg4ODc5Nn0.mto26keeUum_7SEN0fYmS80F9LrqeYBbyIMtPx0GNkE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
