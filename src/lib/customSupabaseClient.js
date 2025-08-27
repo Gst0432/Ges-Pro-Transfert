5a8ff383620d079578b12aeb86372829999beae8
unknown <GSTARTUP>
2025-08-27 17:42:44 +0100

    [dyad] Mise à jour des clés API Supabase - wrote 1 file(s)

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zycqibjgtqfuzbuapyoa.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5Y3FpYmpndHFmdXpidWFweW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNzMxNTMsImV4cCI6MjA3MTY0OTE1M30.NbtnrYt4aZAB8e7Owg8WhFl6Vf31A11-wmM5GsIKVnQ';
+const supabaseUrl = 'https://lueeptijffpwedouxylf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1ZWVwdGlqZmZwd2Vkb3V4eWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI3OTYsImV4cCI6MjA3MTg4ODc5Nn0.mto26keeUum_7SEN0fYmS80F9LrqeYBbyIMtPx0GNkE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
