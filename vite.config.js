31ea0ef0ec4d63bef33ac70d1848d3b7246e311c
PPro1 <premiumproser@gmail.com>
2025-08-29 20:35:29 +0000

    Fix JSX syntax error by configuring esbuild loader

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
